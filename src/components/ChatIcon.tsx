36e8f33441cd95c43efe925cd47161f817e652a5
nahid177 <nahidulislamshantogo@gmail.com>
2024-11-30 01:48:54 +0600

    details

--- src/components/ChatIcon.tsx
+++ src/components/ChatIcon.tsx
@@ -6,8 +6,8 @@ const ChatIcon = () => {
   return (
     <Link href="/contactUs">
       <div
-        className="fixed bottom-14 right-0 mb-4 mr-4 bg-blue-500 p-3 rounded-full shadow-lg hover:bg-blue-600 transition-all duration-300 chat-icon"
-        aria-label="Chat with us"
+        className="tooltip tooltip-close tooltip-left  fixed bottom-14 right-0 mb-4 mr-4 bg-blue-500 p-3 rounded-full shadow-lg hover:bg-blue-600 transition-all duration-300 chat-icon"
+        aria-label="Chat with us " data-tip="Chat with Admin"
       >
         <FaComment className="text-white text-3xl" />
       </div>
